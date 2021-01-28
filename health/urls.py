6092f440fd501d64c148360be824ae45f9008f39
Felroy <felroy01998@gmail.com>
2021-01-28 15:15:02 +0000

    frontend

--- health/urls.py
+++ health/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [    
     path('admin/', admin.site.urls),
-    path('', include('index.urls')), 
+    path('', include('index.urls', namespace = "index")), 
     path('symp/', include('symp.urls')),
 ]
--- index/urls.py
+++ index/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path, re_path
 from . import views
 
+app_name = 'index'
+
 urlpatterns = [   
     # / 
     path('', views.index, name='index'),
